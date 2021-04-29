2df79daffc92f10016dd345099bc5fe7ae62da4d
datdofpt <dat.doxuan@ggg.com.vn>
2021-04-29 20:37:13 +0700

    1a

---
+++ ngay1/001/index6.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    echo "<h1>Thẻ h1 bằng php</h1>";
+    echo "<br>";
+    echo "chuỗi bình thường";
+    echo "<br>";
+    echo 'chuỗi bình thường';
+    echo "<br>";
+    // hà nội là 'thủ đô' của việt nam
+    // cách 1 là dùng nháy kép chứa nháy đơn ok
+    echo "hà nội là 'thủ đô' của việt nam";
+    echo "<br>";
+    // cách 2 dùng dấu escape trước nháy đơn bên trong
+    echo 'hà nội là \'thủ đô\' của việt nam';
+    // trong php sử dụng nháy kép hay nháy đơn để chứa 1 chuỗi
+    // điều gì xảy ra nếu bản thân chuỗi chứa nháy kép hay nháy đơn
+    ?>
+
+</body>
+</html>
---
+++ ngay1/001/index7.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    // hà nội là "thủ đô" của việt nam
+    // cách 1 dùng nháy đơn chứa nháy kép
+    echo "<br>";
+    echo 'hà nội là "thủ đô" của việt nam';
+    echo "<br>";
+    // cách 2 dùng dấu escape \
+    echo "hà nội là \"thủ đô\" của việt nam";
+    ?>
+
+</body>
+</html>
