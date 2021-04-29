c8dfe3c33d3beb024a0ee0c146b1bda06911a6ec
datdofpt <dat.doxuan@ggg.com.vn>
2021-04-29 19:30:19 +0700

    111

--- ngay1/001/index1.php
+++ ngay1/001/index1.php
@@ -1,1 +1,13 @@
 <?php
+// các lệnh php phải nằm trong script mở và script đóng
+// url : http://localhost/LPHP2103E/ngay1/001/index1.php
+// đường dẫn trên ổ đĩa : C:\xampp\htdocs\LPHP2103E\ngay1\001/index1.php
+
+// 1 - file php phải đặt htdocs của xampp
+// 2 - chuyển đường dẫn trên ổ đĩa thành đường dẫn url để chạy trong trình duyệt
+// C:\xampp\htdocs\ == http://localhost/
+// LPHP2103E\ngay1\001/index1.php => thay tất cả \ thành /
+// LPHP2103E/ngay1/001/index1.php
+// http://localhost/LPHP2103E/ngay1/001/index1.php
+echo "chào php buổi 1";
+?>
---
+++ ngay1/abcd/hi.php
@@ -0,0 +1,4 @@
+<?php
+// C:\xampp\htdocs\LPHP2103E\ngay1\abcd\hi.php
+// http://localhost/LPHP2103E/ngay1/abcd/hi.php
+echo "hi php";
