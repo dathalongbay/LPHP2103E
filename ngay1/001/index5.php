a44526c26001f7e67510dc7c57de3e54d6e3424d
datdofpt <dat.doxuan@ggg.com.vn>
2021-04-29 20:19:30 +0700

    11

---
+++ ngay1/001/index5.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+        $ten = "nguyễn văn an";
+    $tuoi = 21;
+    $diem = 8.5;
+    // mảng
+    $thanhPho = ["hà nội", "hồ chí minh", "đà nẵng"];
+    // kiểu null
+    $e = null;
+    // kiểu boolean
+    $gioiTinh = true;
+    // kiểu object
+    class SinhVien {
+
+    }
+    // object
+    $nam = new SinhVien();
+
+    // hàm var_dump(); để kiểm tra kiểu du liệu của biến
+
+var_dump($ten);
+var_dump($tuoi);
+var_dump($diem);
+var_dump($thanhPho);
+var_dump($gioiTinh);
+var_dump($nam);
+
+
+
+
+?>
+
+
+<script>
+    // kiểu string
+    var t = "abc";
+    // kiểu số
+    var a = 5;
+    // kiểu boolean
+    var c = true;
+    var d = false;
+    // kiểu object
+    var e = {};
+    // kiểu mảng
+    var f = [];
+    // kiểu undefined
+    var m;
+</script>
+</body>
+</html>
